5d750fc86898c450b0b0c61b9845242df8ef0741
yunaichun <yunaichun@qingting.fm>
2019-11-04 02:14:39 +0800

    createStackNavigator 创建导航

--- App.js
+++ App.js
@@ -10,13 +10,19 @@ import React from 'react';
 import {
   View,
 } from 'react-native';
-// import Demo from './lib/demo/01/AppCopy';
-import Demo from './lib/demo/01/flex';
+/* 引入导航器 */
+import { createAppContainer } from 'react-navigation';
+
+
+/* 1、脚手架生成的初始页面 */
+// import Index from './lib/demo/cliInit/index';
+/* 2、flex 布局 */
+import Index from './lib/demo/flex/index';
 
 const App: () => React$Node = () => {
   return (
     <View style={{marginTop: 20,}}>
-      <Demo/>
+      <Index/>
     </View>
   );
 };
--- index.js
+++ index.js
@@ -3,7 +3,14 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+/* 通过 App 组件引入的组件 */
+import App from './App';
+/* 引入导航器 */
+import { createAppContainer } from 'react-navigation';
+/* 引入导航路由 */
+import { AppStackNavigator } from './lib/demo/createStackNavigator/index';
 
-AppRegistry.registerComponent(appName, () => App);
+const AppContainer = createAppContainer(AppStackNavigator);
+
+AppRegistry.registerComponent(appName, () => AppContainer);

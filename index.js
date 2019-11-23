a9767de0a9035ac9ba98f320facbe56b834ad8d6
yunaichun <yunaichun@qingting.fm>
2019-11-24 03:00:52 +0800

    createSwitchNavigator 创建不能右滑返回路由

--- index.js
+++ index.js
@@ -4,14 +4,24 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-/* 通过 App 组件引入的组件【非导航组件的 demo 启用此项查看】
-import App from './App';
- */
+
 /* 引入导航器 */
 import { createAppContainer } from 'react-navigation';
-/* 引入导航路由 */
-import { AppStackNavigator } from './lib/demo/navigator/createStackNavigator/index';
 
+/* 一、通过 App 组件引入的组件【非导航组件的 demo 启用此项查看】 
+import AppContainer from './App';
+AppRegistry.registerComponent(appName, () => AppContainer);
+*/
+
+/* 二、引入stack、top、bottom导航路由
+import { AppStackNavigator } from './lib/demo/navigator/createStackNavigator/index';
 const AppContainer = createAppContainer(AppStackNavigator);
+
+AppRegistry.registerComponent(appName, () => AppContainer);
+ */
+
+/* 三、引入switch导航路由 */
+import { AppSwitchNavigator } from './lib/demo/navigator/createSwitchNavigator/index';
+const AppContainer = createAppContainer(AppSwitchNavigator);
 
 AppRegistry.registerComponent(appName, () => AppContainer);

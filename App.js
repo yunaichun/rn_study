a0abb420bc580a66704e7f7ca5156063614abe29
yunaichun <yunaichun@qingting.fm>
2019-12-21 23:10:06 +0800

    添加 redux 状态管理

--- App.js
+++ App.js
@@ -1,30 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {
-  View,
-} from 'react-native';
-/* 引入导航器 */
+// == 引入react-navigation
 import { createAppContainer } from 'react-navigation';
+// == 引入redux
+import { Provider } from 'react-redux';
+import store from './lib/src/store';
 
+// == 一、flex 布局
+// import Index from './lib/demo/flex/index';
+// const AppContainer = () => <Index/>;
 
-/* 1、脚手架生成的初始页面 */
-// import Index from './lib/demo/initinalApp.js/index';
-/* 2、flex 布局 */
-import Index from './lib/demo/flex/index';
+// == 二、引入stack、top、bottom导航路由
+// import { AppStackNavigator } from './lib/demo/navigator/createStackNavigator/index';
+// const AppContainer = createAppContainer(AppStackNavigator);
 
-const App: () => React$Node = () => {
+// == 三、引入switch导航路由 
+// import { AppSwitchNavigator } from './lib/demo/navigator/createSwitchNavigator/index';
+// const AppContainer = createAppContainer(AppSwitchNavigator);
+
+// == 四、高性能列表组件 
+// import { AppStackNavigator } from './lib/demo/list/StackNavigator/index';
+// const AppContainer = createAppContainer(AppStackNavigator);
+
+// == 五、当前项目
+import { AppWithNavigationState } from './lib/src/routers/index';
+const AppContainer = createAppContainer(AppWithNavigationState);
+
+const App = () => {
   return (
-    <View style={{marginTop: 20,}}>
-      <Index/>
-    </View>
+    <Provider
+    store={store}
+    >
+      <AppContainer/>
+    </Provider>
   );
 };
 
 export default App;
+
--- index.js
+++ index.js
@@ -3,37 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
+import App from './App';
 import {name as appName} from './app.json';
 
-/* 引入导航器 */
-import { createAppContainer } from 'react-navigation';
-
-/* 一、通过 App 组件引入的组件【非导航组件的 demo 启用此项查看】 
-import AppContainer from './App';
-AppRegistry.registerComponent(appName, () => AppContainer);
-*/
-
-/* 二、引入stack、top、bottom导航路由
-import { AppStackNavigator } from './lib/demo/navigator/createStackNavigator/index';
-const AppContainer = createAppContainer(AppStackNavigator);
-
-AppRegistry.registerComponent(appName, () => AppContainer);
-*/
-
-/* 三、引入switch导航路由 
-import { AppSwitchNavigator } from './lib/demo/navigator/createSwitchNavigator/index';
-const AppContainer = createAppContainer(AppSwitchNavigator);
-
-AppRegistry.registerComponent(appName, () => AppContainer);
-*/
-
-/* 四、高性能列表组件 
-import { AppStackNavigator } from './lib/demo/list/StackNavigator/index';
-const AppContainer = createAppContainer(AppStackNavigator);
-AppRegistry.registerComponent(appName, () => AppContainer);
-*/
-
-/* 五、项目 */
-import { AppSwitchNavigator } from './lib/src/routers/index';
-const AppContainer = createAppContainer(AppSwitchNavigator);
-AppRegistry.registerComponent(appName, () => AppContainer);
+AppRegistry.registerComponent(appName, () => App);
